485193a187df29148a91e392db0e6e6d1b053a9d
FDWraith <kevinzhang3702@gmail.com>
2018-12-04 01:38:30 -0500

    completed part 1

--- 2018/day4/sol1.py
+++ 2018/day4/sol1.py
@@ -32,4 +32,25 @@ for line in lines:
                 guard_sleep_table[currentGuard][i] += 1
 
 # testing purposes
-print guard_sleep_table
+# print guard_sleep_table
+
+def sort_key(key):
+    return sum(guard_sleep_table[key])
+
+# find the sleepiest guard
+results = sorted(guard_sleep_table, key = sort_key)
+guard = results[-1]
+
+
+# find the minute that guard is asleep most often
+maxDex = 0
+maxVal = guard_sleep_table[guard][0]
+for i in range(1, 60):
+    if guard_sleep_table[guard][i] > maxVal:
+        maxVal = guard_sleep_table[guard][i]
+        maxDex = i
+
+# Compte the final number
+print guard
+print maxDex
+print guard * maxDex
