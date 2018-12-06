685ebc2a4625d39670a501ee999f9d5bf07bf9d3
FDWraith <kevinzhang3702@gmail.com>
2018-12-06 02:27:20 -0500

    second attempt at part 1, testing on sample input

--- 2018/day6/sol1.py
+++ 2018/day6/sol1.py
@@ -1,8 +1,12 @@
 with open("input.txt", "r") as f:
     lines = f.readlines()
     lines = [line.strip().split(", ") for line in lines]
-    lines = [[int(line[0]), int(line[1])] for line in lines]
+    lines = [tuple([int(line[0]), int(line[1])]) for line in lines]
     f.close()
+
+'''
+First attempt
+
 
 # Using a dictionary structure
 # key : tuple representing coord
@@ -45,3 +49,55 @@ while hasNone(grid):
         next_plot[k] = k_next
     
 
+'''
+
+# Second Attempt
+
+# Use the same grid, but this time let coordinate figure out for themselves, which one they are closest to
+
+grid = [[None for i in range(0, 400)] for j in range(0, 400)]
+
+def manhattanDist(t1, t2):
+    return abs(t1[0] - t2[0]) + abs(t1[1] - t2[1])
+
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+        closestSoFar = lines[0]
+        closestDist = manhattanDist((i,j), lines[0])
+        for line in lines:
+            # ignore first, because done already
+            if line != lines[0]:
+                d = manhattanDist((i,j), line)
+                if d == closestDist:
+                    grid[i][j] = "."
+                elif d < closestDist:
+                    closestDist = d
+                    closestSoFar = line
+        if grid[i][j] == None:
+            grid[i][j] = closestSoFar
+
+# Exclude the ones on the edges, because those will go out to infinity
+excluded = set()
+for item in grid[0]:
+    excluded.add(item)
+for item in [row[0] for row in grid]:
+    excluded.add(item)
+for item in grid[-1]:
+    excluded.add(item)
+for itme in [row[-1] for row in grid]:
+    excluded.add(item)
+
+# exclude the excluded set
+lines = [line for line in lines if line not in excluded]
+
+# count up the area
+final_counts = {}
+for line in lines:
+    final_counts[line] = 0
+
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+        if grid[i][j] in final_counts:
+            final_counts[grid[i][j]] += 1
+
+print max(final_counts.values())
